7230754d764d6142df8bac5ec68e0e6c1181c3b6
munafs2301 <marvelousfrank5@gmail.com>
2020-04-12 16:07:40 +0100

    Initial Code

---
+++ GuessANumber.py
@@ -0,0 +1,22 @@
+'''
+Program to guess a number between 1 and 9
+''' 
+
+print("-*-" * 51)
+def number_guess():
+    try:
+        number = int(input("\nGuess a number: "))
+        random_number = range(1, 10)
+
+        if number in random_number:
+            print(f"\nWell guessed. The number is {number}\n")
+        
+        else:
+            print("\nWrong!")
+            number_guess()
+    except:
+        print("Invalid input! Try again.")
+        number_guess()
+
+number_guess()
+print("-*-" * 51)
