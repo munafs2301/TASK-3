d3e811865b73fcdbe533a3658e52ab5b4a12bb74
munafs2301 <marvelousfrank5@gmail.com>
2020-04-13 01:19:24 +0100

    Added a docstring to the function

--- PasswordValidator.py
+++ PasswordValidator.py
@@ -4,17 +4,18 @@ A program that checks for password validity
 
 import re
 
-numbers = "[0 1 2 3 4 5 6 7 8 9 ]"
-cap_letters = '[A B C D E F G H I J K L M N O P Q R S T U V W X Y Z]'
+numbers = "[0-9]"
+cap_letters = '[A-Z]'
 print("PASSWORD CREATION RULES \n 1. It must be more than than 8 characters \n 2. It must contain a capital letter \n 3. It must a contain number")
 
 
 def password_validator():
+    ''' Password Validation that checks for length, presence of a capital letter and presence of a number'''
     password = input("\nPlease enter a password: ")
 
     if len(password) > 8:
         if re.search(numbers, password):
-            if re.search(cap_letters, password,):
+            if re.search(cap_letters, password):
                     print("\nYou have succefully created your password")
             else:
                 print("There is no capital letter")
